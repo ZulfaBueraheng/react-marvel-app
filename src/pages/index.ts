4f00ba14027d4d9eac8589a195165cd46f0acd36
ZulfaBueraheng <zulfa.bg@gamil.com>
2025-03-22 03:01:52 +0700

    Heroes page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,5 @@
-import Home from "./Home";// Ensure Heroes is imported
+import Home from "./Home";
+import Heroes from "./Heroes"; // Ensure Heroes is imported
+import HeroesDetail from "./HeroesDetail";
 
-export { Home }; // Export both Home and Heroes
+export { Home, Heroes, HeroesDetail }; // Export both Home and Heroes
